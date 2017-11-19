819f7d23b74c18f6ff28c9c3effa7ad0f27d34a1
Noah Tajwar <noah.tajwar.oss@gmail.com>
2017-11-19 11:16:58 -0800

    Updated to put PNL to LOW instead of PWR to HIGH

--- TX2_Power_Controller/TX2_Power_Controller.ino
+++ TX2_Power_Controller/TX2_Power_Controller.ino
@@ -1,15 +1,63 @@
-#define LED_PIN 7
+#define PIN_POWER_TRANSISTOR 13
+#define PIN_PNL 7
 
 #define SIGNAL_POWER_CYCLE "POWER_CYCLE"
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(PIN_POWER_TRANSISTOR, OUTPUT);
+  pinMode(PIN_PNL, OUTPUT);
+  
   Serial.begin(9600);
+  Serial1.begin(9600);
+
+  digitalWrite(PIN_PNL, HIGH);
+  digitalWrite(PIN_POWER_TRANSISTOR, HIGH);
+}
+
+void cycleTX2Power()
+{
+  // "Disconnect" the power supply
+  Serial.println("Turning power supply OFF");
+  digitalWrite(PIN_POWER_TRANSISTOR, LOW);
+  
+  // Wait for power to turn off
+  delay(5000);
+  
+  // "Connect" the power supply
+  Serial.println("Turning power supply ON...");
+  digitalWrite(PIN_POWER_TRANSISTOR, HIGH);
+  
+  // Wait for power supply to "connect"
+  delay(1000);
+  
+  // "Press" the "power button"
+  Serial.println("Setting PNL to LOW...");
+  digitalWrite(PIN_PNL, LOW);
+  
+  // Hold the "power button" down for a little bit
+  delay(2500);
+  
+  // "Release" the "power button"
+  Serial.println("Setting PNL to HIGH");
+  digitalWrite(PIN_PNL, HIGH);
 }
 
 void loop()
 {
+  /*int serialByte;
+  int serial1Byte;
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+  }
+  
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
+  }
+  
+  Serial.write(inByte);*/
+  
   String readString;
   
   while(Serial.available())
@@ -25,16 +73,7 @@ void loop()
       {
         if(readString == SIGNAL_POWER_CYCLE)
         {
-          Serial.println("Cycling Power on TX2...");
-          for(int i = 0; i < 5; i++)
-          {
-            // TODO actually trigger a power cycle
-            // For now just flash an LED
-            digitalWrite(LED_PIN, HIGH);
-            delay(100);
-            digitalWrite(LED_PIN, LOW);
-            delay(100);
-          }
+          cycleTX2Power();
         }
         else
         {
